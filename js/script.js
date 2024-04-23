bd39c7e0dfc4956bc0c9f07d784fabc392b0db32
arth2101pv <arthur.scabral@senacsp.edu.br>
2024-04-23 16:48:55 -0300

    Finalizando

--- js/script.js
+++ js/script.js
@@ -1,63 +1,52 @@
-const container = document.querySelector(".container");
-
+const container =  document.querySelector(".container");
 const qrCodeBtn = document.querySelector("#qr-form button");
-
-const qrCodeInput = container.querySelector("#qr-form input");
-
+const qrCodeInput=  container.querySelector("#qr-form input");
 const qrCodeImg = container.querySelector("#qr-code img");
 
 const downloadBtn = document.getElementById('download-btn');
 
 function generateQrCode(){
     let qrCodeInputValue = qrCodeInput.value;
-
     if(!qrCodeInputValue) return;
-
-    qrCodeBtn.innerText = "Gerando QR Code..."
-
+    qrCodeBtn.innerText = "Gerando código..."
     qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
-
-    qrCodeImg.addEventListener("load", () => {
+    qrCodeImg.addEventListener("load", () =>{
         container.classList.add("active");
-        qrCodeBtn.innerText = "QR Code criado"
+        qrCodeBtn.innerText = "Código criado";
     })
 }
 
-qrCodeBtn.addEventListener("click", () => {
+qrCodeBtn.addEventListener("click", ()  => {
     generateQrCode();
-})
+});
+
 
 qrCodeInput.addEventListener("keydown", (e) => {
     if (e.code === "Enter") {
-        generateQrCode();
+      generateQrCode();
     }
-});
+  });
 
-qrCodeInput.addEventListener("keyup", () => {
-    if (!qrCodeInput.value) {
+  qrCodeInput.addEventListener("keyup", () =>{
+    if(!qrCodeInput.value){
         container.classList.remove("active");
         qrCodeBtn.innerText = "Gerar QR Code";
     }
-})
+  })
 
-function downloadQR() {
+
+  function downloadQR() {
     // URL da imagem gerada
-    let imgUrl = qrCodeImg.src;
+    let imgUrl = qrCodeImg.src;  
     // Criar elemento <a> para download
     let link = document.createElement('a');
     link.href = imgUrl;
     // Definir nome do arquivo
-    link.download = "qrcode.png";
+    link.download = 'qrcode.png';  
     // Simular click para iniciar download
     link.click();
-}
+  }
 
-downloadBtn.addEventListener("click", () => {
+  downloadBtn.addEventListener('click', () => {
     downloadQR();
-});
-
-function generateQrCode() {
-    // Gerar código
-    
-    downloadBtn.style.display = "block"
-}
+  });
